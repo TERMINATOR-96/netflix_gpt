c47978c76a1fb0d706c151615fd8b22aee53a0c2
Rahul Pandey <rahulpandey1996vns@gmail.com>
2025-01-07 00:34:12 +0530

    Added video title to main container

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,4 +1,6 @@
 import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
@@ -6,6 +8,16 @@ const Browse = () => {
     return (
         <div>
             <Header />
+            {/**
+             * MainContainer
+             *  - VideoBackground
+             *  - VideoTitle
+             * SecondaryContainer
+             * - MovieList * n
+             *    - Cards * n
+             */}
+             <MainContainer />
+             <SecondaryContainer />
         </div>
     );
 };
