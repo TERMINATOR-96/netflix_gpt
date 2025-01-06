bafe270c61f6f15439daf07ec2d0b0f7cc08034b
Rahul Pandey <rahulpandey1996vns@gmail.com>
2025-01-06 15:04:35 +0530

    Added a custom hook for fetching data from TMDB and updating store

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,26 +1,8 @@
 import Header from "./Header";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { API_OPTIONS } from "../utils/constants";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-    const dispatch = useDispatch();
-
-    //api call to fetch data from TMDB api
-    const getNowPlayingMovies = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-        const json = await data.json();
-        console.log(json.results);
-        //dispatch an action
-        dispatch(addNowPlayingMovies(json.results));
-    }
-
-    //we have to call this api only once when the component is rendered
-    useEffect(() => {
-        getNowPlayingMovies();
-    }, []); //empty square brackets we want to call this api only once
-
+    useNowPlayingMovies();
     return (
         <div>
             <Header />
---
+++ netflix-gpt/src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+
+//hooks is a normal javascript function
+const useNowPlayingMovies = () => {
+    //fetch data from TMDB API and update store
+    const dispatch = useDispatch();
+
+    //api call to fetch data from TMDB api
+    const getNowPlayingMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+        const json = await data.json();
+        console.log(json.results);
+        //dispatch an action
+        dispatch(addNowPlayingMovies(json.results));
+    }
+
+    //we have to call this api only once when the component is rendered
+    useEffect(() => {
+        getNowPlayingMovies();
+    }, []); //empty square brackets we want to call this api only once
+};
+
+export default useNowPlayingMovies;
